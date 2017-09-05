69ebda273bca11f25056939c402885d164a038ce
Nicole Jeung <ngj760@gmail.com>
2017-09-05 11:48:05 -0700

    flex time

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,12 @@
+
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(5));
+		System.out.println(Calculate.cube(5));
+		System.out.println(Calculate.average(2,4));
+
+
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,15 @@
+// Nicole Jeung
+// September 5, 2017
+// This will take in an integer and return its square
+public class Calculate {
+	public static int square(int number) {
+		return number * number;
+	}
+	public static int cube(int number) {
+		return number * number * number;
+	}
+	public static double average(double number1, double number2)  {
+		return (number1 + number2)/2;
+	}
+
+}
