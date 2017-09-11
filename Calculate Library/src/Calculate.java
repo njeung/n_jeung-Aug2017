468a3dde8e6b581d40c075e635da832d3dba22da
Nicole Jeung <ngj760@gmail.com>
2017-09-11 08:45:58 -0700

    mon

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -11,5 +11,28 @@ public class Calculate {
 	public static double average(double number1, double number2)  {
 		return (number1 + number2)/2;
 	}
-
+	public static double average2(double number1, double number2, double number3) {
+		return (number1 + number2 + number3)/3;
 }
+	public static double toRadians(double number) {
+		return (number * (3.14159/180));
+	}
+	public static double toDegrees(double number) {
+		return (number * (180/3.14159));
+	}
+	public static double discriminant(double number1, double number2, double number3) {
+	return ((number1*number1)-(4*number2*number3));
+	}
+	public static String toImproperFrac(int number1, int number2, int number3) {
+		return (((number1 * number3) + number2+"/"+number3));
+	}
+	public static String toMixedNum(int number1, int number2) {
+		return number1/number2+"_"+ number1%number2+ "/"+ number2;
+	}
+	public static String foil(int number1, int number2, int number3, int number4, String variable) {
+		return number1*number3+variable+"^2"+"+" + ((number1*number4)+(number2*number3))+variable+"+"+ (number2*number4);
+	}
+}
+
+
+
